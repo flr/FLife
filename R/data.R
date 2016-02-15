70ce9d235f506a422f4c065f036114fd9b53ebba
laurieKell <laurie@kell.es>
2016-02-15 12:06:32 +0100

    help for data

--- R/data.R
+++ R/data.R
@@ -1,18 +1,37 @@
-#' Prices of 50,000 round cut diamonds.
+#' teleost
+#'
+#' A dataset containing life history parameters for a range of teleost species
 #' 
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' teleost.
-#' 
-#' format gives an overview of the dataset. For data frames, you should include a definition list that describes each variable. It’s usually a good idea to describe variables’ units here.
-#' 
-#' source provides details of where you got the data, often a \url{}.
-#'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 139 rows (i.e. species) and 13 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
+#'   \item{species}{}
+#'   \item{order}{}   
+#'   \item{genus}{}   
+#'   \item{family}{} 
+#'   \item{class}{} 
+#'   \item{linf}{asymptotic length of the Von Bertalanffy growth equation}    
+#'   \item{k}{rate at which length reaches linf}       
+#'   \item{t0}{adjust the growth equation for the initial size}      
+#'   \item{l50}{length at which 50% are mature}     
+#'   \item{a}{scaling factor of the length weight relationship}       
+#'   \item{b}{exponent of the length weight relationship}       
+#'   \item{temp}{temperature}    
+#'   \item{habit}{e.g. pelagic or demeresal}   
 #' }
-#' @source \url{http://www.diamondse.info/}
-"diamonds"
+"teleost"
+
+
+#' lengths
+#'
+#' A dataset containing lengths by year
+#'
+#' @format A data frame with 139 rows (i.e. species) and 13 variables:
+#' \describe{
+#'   \item{length}{}
+#'   \item{year}{}   
+#'   \item{data}{}   
+#' }
+#' @source \url{http://iccat.int/en/accesingdb.htm}
+"lengths"
+
